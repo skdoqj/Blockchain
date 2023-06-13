b5984a3cbb318f8e07e2d188402c86fe6eb31683
suwan <skdoqj@naver.com>
2023-06-13 18:11:16 +0900

    인터페이스로 변경

--- 4_3_interface.ts
+++ 4_3_interface.ts
@@ -1,14 +1,27 @@
-abstract class User {
-  constructor(protected firstName: string, protected lastName: string) {}
-  abstract sayHi(name: string): string;
-  abstract fullName(): string;
+interface User {
+  firstName: string;
+  lastName: string;
+
+  sayHi(name: string): string;
+  fullName(): string;
 }
 
-class Player extends User {
+interface Human {
+  health: number;
+}
+//extends 대신 implements, interface를 중복으로 상속할 수 있다
+class Player implements User, Human {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public health: number
+  ) {}
   fullName() {
     return `${this.firstName} ${this.lastName}`;
   }
   sayHi(name: string) {
-    return "hi" + name + "my name is" + this.fullName;
+    return `hi ${name} my name is ${this.fullName}`;
   }
 }
+
+const gg = new Player("suwan", "park", 70);
