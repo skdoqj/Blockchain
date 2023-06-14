2685f6a7a044c9a47f8bb9ca5d0882beda3052dc
Suwan <skdoqj@naver.com>
2023-06-14 23:28:43 +0900

    5.5 Block

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-import { init, exit } from './myPackage';
+// import crypto from "crypto";
+import * as crypto from "crypto";
 
-init({debug:true ,url:"www"})
-exit(123)
+interface BlockShape {
+    hash: string;
+    prevHash: string;
+    height: number;
+    data: string;
+}
+
+class Block implements BlockShape {
+    public hash: string;
+    constructor(
+        public prevHash: string,
+        public height: number,
+        public data: string
+    ){
+        this.hash = Block.calculateHash(prevHash, height, data) //hash 초기화
+    }
+    static calculateHash(prevHash: string, height: number, data:string){
+        const toHash = `${prevHash}${height}${data}`
+    }
+}
---
+++ static.ts
@@ -0,0 +1,3 @@
+class foo{
+    public static
+}
