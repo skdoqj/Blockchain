f12c652e25cd2e19ee276430a187b6e617205dd2
jaehan <keompyuteo@JJJJ.local>
2023-06-14 20:52:52 +0900

    4.4 JSDoc

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { init, exit } from 'myPackage'
+import { init, exit } from './myPackage';
 
-init({url:"www"})
+init({debug:true ,url:"www"})
 exit(123)
--- src/myPackage.js
+++ src/myPackage.js
@@ -1,6 +1,21 @@
+// @ts-check
+
+/**
+ * Initializes the project
+ * @param {object} config  //{입력값} 입력값이름
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns {boolean}
+ */
 export function init(config){
     return true;
 }
+
+/**
+ * Exit the program
+ * @param {number} code 
+ * @returns {number}
+ */
 export function exit(code) {
     return code +1 ;
 }
